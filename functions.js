e409ba6079a688250e5f4d4be4f9f3db76752963
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-09-11 09:37:42 +0300

    Making stickers...

--- functions.js
+++ functions.js
@@ -2,34 +2,43 @@
 
 function getPhrase(phrases, rsReply) {
     /*
-    * Function gets a reply from RiveScript 'brain' and a dictionary with phrases
-    * and replaces 'phrase1/phrase10/phraseN' with corresponding phrases from L.Podervjansky's stories
+    * Function parses a reply from RiveScript 'brain' which (reply) can include
+    * a) only text,
+    * b) only a sticker or
+    * c) text + sticker.
+    * Only 1 sticker may be in each reply
     * */
-    console.log(`getPhrase: rsReply - ${rsReply}`);
-    let rsReplySplit = rsReply.split(' ');
-    let phraseN = 0;
-    for (let i=0; i<rsReplySplit.length; i++) {
-        if (rsReplySplit[i].includes('phrase')) {
-            try {
-                phraseN = parseInt(rsReplySplit[i].split('phrase')[1]);
-            } catch(err) {
-                console.log(`Error: ${err}`);
-            }
-            if (phraseN!=0) {
-                if  (phrases.hasOwnProperty(phraseN)) {
-                    return rsReply.replace(`phrase${phraseN}`, `\n\n${phrases[phraseN]}`);
-                    console.log(`Substituted`);
-                } else {
-                    return rsReply;
+    console.log(`rsReply: ${rsReply}`);
+    let output = {"text": false, "sticker": false};
+
+    if (rsReply.includes('phrase')) {
+        if (rsReply.split('phrase')[0] !== '') {
+            output["text"] = rsReply.split('phrase')[0];
+        }
+
+        let rsReplySplit = rsReply.split(' ');
+        let phraseN = 0;
+        for (let i=0; i<rsReplySplit.length; i++) {
+            if (rsReplySplit[i].includes('phrase')) {
+                try {
+                    phraseN = parseInt(rsReplySplit[i].split('phrase')[1]);
+
+                    if (phraseN!=0) {
+                        if  (phrases.hasOwnProperty(phraseN)) {
+                            output["sticker"] = phrases[phraseN];
+                        }
+                    }
+                } catch(err) {
+                    console.log(`>> getPhrase: ${err}`);
                 }
-            } else {
-                return rsReply;
-                console.log(`Without changes`);
             }
         }
+    } else {
+        output["text"] = rsReply;
     }
-    return rsReply;
+    return output;
 }
+
 
 module.exports = {
     getPhrase
