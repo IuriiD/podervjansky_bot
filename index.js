8814e745d1379dadd35a1a9441906c281f4c17c0
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-10-29 17:14:28 +0200

    Code update

--- index.js
+++ index.js
@@ -1,76 +1,78 @@
-'use strict';
-
+require('dotenv').config();
 const RiveScript = require('rivescript');
 const Telegraf = require('telegraf');
-const Markup = require('telegraf/markup');
 const Extra = require('telegraf/extra');
 
-const keys = require('./keys');
 const f = require('./functions');
 const phrases = require('./phrases');
 
-const bot = new Telegraf(keys.TELEGRAM_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
-const riveBot = new RiveScript({utf8: true});
+const riveBot = new RiveScript({ utf8: true });
 const punctuation = new RegExp(/[.,!;:]/g);
 
-riveBot.loadDirectory('./brain').then(loading_done).catch(loading_error);
+riveBot
+  .loadDirectory('./brain')
+  .then(loading_done)
+  .catch(loading_error);
 
-function loading_done (batch_num) {
-    console.log('Bot is ready!');
-    riveBot.sortReplies();
+function loading_done(batch_num) {
+  console.log('Bot is ready!');
+  riveBot.sortReplies();
 }
 
-function loading_error (error) {
-    console.log("Error when loading files: " + error);
+function loading_error(error) {
+  console.log(`Error when loading files: ${error}`);
 }
 
-bot.command(['start'], async ctx => {
-    const userId = ctx.from.id;
-    const username = ctx.from.first_name;
-    let replyFromRS = await riveBot.reply(userId, username);
-    let replyWithSubs = f.getPhrase(phrases, replyFromRS);
-    console.log(`\n\n\n${JSON.stringify(replyWithSubs)}`);
-    if (replyWithSubs.text) {
-        await ctx.replyWithHTML(replyWithSubs.text);
-    }
-    if (replyWithSubs.sticker) {
-        await ctx.replyWithSticker('CAADAgADBAAD09KeCnfMl_xO0kSQAg', Extra.HTML().markup((m) =>
-            m.inlineKeyboard([
-                m.callbackButton('Слухати', '#listen'),
-                m.callbackButton('Читати', '#read')
-            ])));
-    }
+bot.command(['start'], async (ctx) => {
+  const userId = ctx.from.id;
+  const username = ctx.from.first_name;
+  const replyFromRS = await riveBot.reply(userId, username);
+  const replyWithSubs = f.getPhrase(phrases, replyFromRS);
+  console.log(`\n\n\n${JSON.stringify(replyWithSubs)}`);
+  if (replyWithSubs.text) {
+    await ctx.replyWithHTML(replyWithSubs.text);
+  }
+  if (replyWithSubs.sticker) {
+    await ctx.replyWithSticker(
+      'CAADAgADBAAD09KeCnfMl_xO0kSQAg',
+      Extra.HTML().markup(m => m.inlineKeyboard([
+        m.callbackButton('Слухати', '#listen'),
+        m.callbackButton('Читати', '#read'),
+      ])),
+    );
+  }
 });
 
-bot.on('text', async ctx => {
-    const userId = ctx.from.id;
-    let replyFromRS = await riveBot.reply(userId, ctx.message.text);
-    let replyWithSubs = f.getPhrase(phrases, replyFromRS);
-    console.log(`\n\n\n${JSON.stringify(replyWithSubs)}`);
-    if (replyWithSubs.text) {
-        await ctx.replyWithHTML(replyWithSubs.text);
-    }
-    if (replyWithSubs.sticker) {
-        await ctx.replyWithSticker('CAADAgADAwAD09KeCpV7FIvkzeffAg');
-        await ctx.replyWithHTML(replyWithSubs.sticker, Extra.HTML().markup((m) =>
-            m.inlineKeyboard([
-                m.callbackButton('Слухати', '#listen'),
-                m.callbackButton('Читати', '#read')
-            ])))
-    }
+bot.on('text', async (ctx) => {
+  const userId = ctx.from.id;
+  const replyFromRS = await riveBot.reply(userId, ctx.message.text);
+  const replyWithSubs = f.getPhrase(phrases, replyFromRS);
+  console.log(`\n\n\n${JSON.stringify(replyWithSubs)}`);
+  if (replyWithSubs.text) {
+    await ctx.replyWithHTML(replyWithSubs.text);
+  }
+  if (replyWithSubs.sticker) {
+    await ctx.replyWithSticker('CAADAgADAwAD09KeCpV7FIvkzeffAg');
+    await ctx.replyWithHTML(
+      replyWithSubs.sticker,
+      Extra.HTML().markup(m => m.inlineKeyboard([
+        m.callbackButton('Слухати', '#listen'),
+        m.callbackButton('Читати', '#read'),
+      ])),
+    );
+  }
 });
 
-
-bot.on('sticker', async ctx => {
-    console.log(ctx.message);
-    await ctx.replyWithHTML(`It was a sticker!`);
+bot.on('sticker', async (ctx) => {
+  console.log(ctx.message);
+  await ctx.replyWithHTML('It was a sticker!');
 });
 
-bot.on('photo', async ctx => {
-    console.log(ctx.message);
-    await ctx.replyWithHTML(`It was a photo!`);
+bot.on('photo', async (ctx) => {
+  console.log(ctx.message);
+  await ctx.replyWithHTML('It was a photo!');
 });
-
 
 bot.startPolling();
--- parser.js
+++ parser.js
@@ -3,53 +3,51 @@
 // Then I'll manually choose phrases-candidates for replies/stickers
 // Texts from http://doslidy.org.ua/?page_id=123
 
-'use strict';
 
 const fs = require('fs');
 
-const doslidy = 'Іржик\n' +
-    '\n' +
-    'Дійові особи:\n' +
-    '\n' +
-    'Іржик – партізан.\n' +
-    'Смєтанка – бойова подруга Іржика.\n' +
-    'Краєчка – друг Іржика, тоже партізан\n' +
-    '\n' +
-    'а також німці і партізани.\n' +
-    '\n' +
-    'Дія перша\n' +
-    '\n' +
-    'В горах. Іржик займає бойову позицію біля пулімьоту сракою догори. На ньому верхи сидить Смєтанка та бавиться.\n' +
-    '\n' +
-    'Смєтанка: Іржик, оно диви, німці!\n' +
-    'Іржик: Де! Тра-та-та-та-та(строчить з пулімьоту).\n' +
-    'Смєтанка: Га-га-га-га-га(регоче).\n' +
-    'Іржик(сам до себе): Знову наїбала, сука.\n' +
-    '\n' +
-    'Входить Дворжик\n' +
-    '\n' +
-    'Дворжик: Іржик, диви, там німці!\n' +
-    'Іржик: Де! Тра-та-та-та-та(строчить з пулімьоту).\n' +
-    'Смєтанка: Га-га-га-га-га! От так наїбав.\n' +
-    '\n' +
-    'З”являються німці.”';
+const doslidy = 'Іржик\n'
+  + '\n'
+  + 'Дійові особи:\n'
+  + '\n'
+  + 'Іржик – партізан.\n'
+  + 'Смєтанка – бойова подруга Іржика.\n'
+  + 'Краєчка – друг Іржика, тоже партізан\n'
+  + '\n'
+  + 'а також німці і партізани.\n'
+  + '\n'
+  + 'Дія перша\n'
+  + '\n'
+  + 'В горах. Іржик займає бойову позицію біля пулімьоту сракою догори. На ньому верхи сидить Смєтанка та бавиться.\n'
+  + '\n'
+  + 'Смєтанка: Іржик, оно диви, німці!\n'
+  + 'Іржик: Де! Тра-та-та-та-та(строчить з пулімьоту).\n'
+  + 'Смєтанка: Га-га-га-га-га(регоче).\n'
+  + 'Іржик(сам до себе): Знову наїбала, сука.\n'
+  + '\n'
+  + 'Входить Дворжик\n'
+  + '\n'
+  + 'Дворжик: Іржик, диви, там німці!\n'
+  + 'Іржик: Де! Тра-та-та-та-та(строчить з пулімьоту).\n'
+  + 'Смєтанка: Га-га-га-га-га! От так наїбав.\n'
+  + '\n'
+  + 'З”являються німці.”';
 
 const storyName = 'Іржик';
 
-let rawPhrases = doslidy.split(/[\n.]/);
-let finalPhrases = {};
-for (let n=0; n<rawPhrases.length; n++) {
-    let phrase = rawPhrases[n].trim();
-    let sliceDot = '';
-    phrase[phrase.length - 1] === '.' ? sliceDot = phrase.slice(0, -1) : sliceDot = phrase;
-    if (sliceDot !== '') {
-        finalPhrases[`["${storyName}", фраза ${n+1}]`] = sliceDot;
-    }
+const rawPhrases = doslidy.split(/[\n.]/);
+const finalPhrases = {};
+for (let n = 0; n < rawPhrases.length; n++) {
+  const phrase = rawPhrases[n].trim();
+  let sliceDot = '';
+  phrase[phrase.length - 1] === '.' ? (sliceDot = phrase.slice(0, -1)) : (sliceDot = phrase);
+  if (sliceDot !== '') {
+    finalPhrases[`["${storyName}", фраза ${n + 1}]`] = sliceDot;
+  }
 }
 
 fs.writeFile(`${storyName}.txt`, JSON.stringify(finalPhrases), (err, data) => {
-   if (err) console.log(`Error: ${err}`);
+  if (err) console.log(`Error: ${err}`);
 });
-
 
 console.log(finalPhrases);
