38f72170597865dcc0bcc2c02ecbc4c267855457
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-10-29 18:59:00 +0200

    Phrases update, code refactoring

--- functions.js
+++ functions.js
@@ -1,45 +1,50 @@
-'use strict';
-
 function getPhrase(phrases, rsReply) {
-    /*
-    * Function parses a reply from RiveScript 'brain' which (reply) can include
-    * a) only text,
-    * b) only a sticker or
-    * c) text + sticker.
-    * Only 1 sticker may be in each reply
-    * */
-    console.log(`rsReply: ${rsReply}`);
-    let output = {"text": false, "sticker": false};
+  /*
+   * Function parses a reply from RiveScript 'brain' which (reply) can include
+   * a) only text,
+   * b) only a sticker or
+   * c) text + sticker.
+   * Only 1 sticker may be in each reply
+  */
+  console.log(`rsReply: ${rsReply}`);
+  const output = { text: false, sticker: false };
 
-    if (rsReply.includes('phrase')) {
-        if (rsReply.split('phrase')[0] !== '') {
-            output["text"] = rsReply.split('phrase')[0];
-        }
-
-        let rsReplySplit = rsReply.split(' ');
-        let phraseN = 0;
-        for (let i=0; i<rsReplySplit.length; i++) {
-            if (rsReplySplit[i].includes('phrase')) {
-                try {
-                    phraseN = parseInt(rsReplySplit[i].split('phrase')[1]);
-
-                    if (phraseN!=0) {
-                        if  (phrases.hasOwnProperty(phraseN)) {
-                            output["sticker"] = phrases[phraseN];
-                        }
-                    }
-                } catch(err) {
-                    console.log(`>> getPhrase: ${err}`);
-                }
-            }
-        }
-    } else {
-        output["text"] = rsReply;
+  if (rsReply.includes('phrase')) {
+    if (rsReply.split('phrase')[0] !== '') {
+      output.text = rsReply.split('phrase')[0];
     }
-    return output;
+
+    const rsReplySplit = rsReply.split(' ');
+    let phraseN = 0;
+    for (let i = 0; i < rsReplySplit.length; i++) {
+      if (rsReplySplit[i].includes('phrase')) {
+        try {
+          phraseN = parseInt(rsReplySplit[i].split('phrase')[1]);
+
+          if (phraseN !== 0) {
+            if (phrases.hasOwnProperty(phraseN)) {
+              output.sticker = phrases.phraseN.telegramStickerId;
+            }
+          }
+        } catch (err) {
+          console.log(`>> getPhrase: ${err}`);
+        }
+      }
+    }
+
+    if (output.text === 'Defaultfallback') {
+      // If user's phrase triggered Default Fallback intent -
+      // try to find plays for this phrase using ElasticSearch
+      // If found list them, if not - present only the sticker
+      // for Fallback Intent
+      output.text = false;
+    }
+  } else {
+    output.text = rsReply;
+  }
+  return output;
 }
 
-
 module.exports = {
-    getPhrase
+  getPhrase,
 };
--- index.js
+++ index.js
@@ -9,52 +9,53 @@ const phrases = require('./phrases');
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
 const riveBot = new RiveScript({ utf8: true });
-const punctuation = new RegExp(/[.,!;:]/g);
 
-riveBot
-  .loadDirectory('./brain')
-  .then(loading_done)
-  .catch(loading_error);
-
-function loading_done(batch_num) {
+function loadingDone() {
   console.log('Bot is ready!');
   riveBot.sortReplies();
 }
 
-function loading_error(error) {
+function loadingError(error) {
   console.log(`Error when loading files: ${error}`);
 }
 
+riveBot
+  .loadDirectory('./brain')
+  .then(loadingDone)
+  .catch(loadingError);
+
 bot.command(['start'], async (ctx) => {
-  const userId = ctx.from.id;
   const username = ctx.from.first_name;
-  const replyFromRS = await riveBot.reply(userId, username);
-  const replyWithSubs = f.getPhrase(phrases, replyFromRS);
-  console.log(`\n\n\n${JSON.stringify(replyWithSubs)}`);
-  if (replyWithSubs.text) {
-    await ctx.replyWithHTML(replyWithSubs.text);
-  }
-  if (replyWithSubs.sticker) {
-    await ctx.replyWithSticker(
-      'CAADAgADBAAD09KeCnfMl_xO0kSQAg',
-      Extra.HTML().markup(m => m.inlineKeyboard([
-        m.callbackButton('Слухати', '#listen'),
-        m.callbackButton('Читати', '#read'),
-      ])),
-    );
-  }
+  const introPhrases = [
+    `Привіт, ${username}. Я <b><bot name></b>! В мені - вся мудрість творів пана Леся`,
+    `Привіт, ${username}. Мене звуть <b><bot name></b>. Я чатбот, що говорить фразами із творів Леся Подерв'янського`,
+  ];
+  const introStickers = ['1', '32', '48'];
+  const randomWelcomePhrase = Math.round(Math.random() * introPhrases.length);
+  const randomWelcomeSticker = Math.round(Math.random() * introStickers.length);
+
+  await ctx.replyWithHTML(introPhrases[randomWelcomePhrase]);
+  await ctx.replyWithSticker(
+    phrases[introStickers[randomWelcomeSticker.toString()]].telegramStickerId,
+    Extra.HTML().markup(m => m.inlineKeyboard([
+      m.callbackButton('Слухати', '#listen'),
+      m.callbackButton('Читати', '#read'),
+    ])),
+  );
 });
 
 bot.on('text', async (ctx) => {
   const userId = ctx.from.id;
   const replyFromRS = await riveBot.reply(userId, ctx.message.text);
+  console.log(`User wrote: ${ctx.message.text}`);
   const replyWithSubs = f.getPhrase(phrases, replyFromRS);
-  console.log(`\n\n\n${JSON.stringify(replyWithSubs)}`);
+  console.log(`\n${JSON.stringify(replyWithSubs)}`);
+
   if (replyWithSubs.text) {
     await ctx.replyWithHTML(replyWithSubs.text);
   }
   if (replyWithSubs.sticker) {
-    await ctx.replyWithSticker('CAADAgADAwAD09KeCpV7FIvkzeffAg');
+    await ctx.replyWithSticker(replyWithSubs.sticker);
     await ctx.replyWithHTML(
       replyWithSubs.sticker,
       Extra.HTML().markup(m => m.inlineKeyboard([
@@ -64,6 +65,8 @@ bot.on('text', async (ctx) => {
     );
   }
 });
+
+// Other message types besides text trigger Default fallback response
 
 bot.on('sticker', async (ctx) => {
   console.log(ctx.message);
